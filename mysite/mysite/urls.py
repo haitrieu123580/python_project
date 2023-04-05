1140d29005f424478cc1440e47c8af0b1b8ef81a
haitrieu123580 <haitrieu123580@gmail.com>
2023-04-05 08:33:26 +0700

    new project stucture

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('webApp/',include('webApp.urls')),
+    path('store/',include('store.urls')),
     path('admin/', admin.site.urls),
 ]
--- mysite/webApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    # ex: /webApp/1
-    path('<int:lap_id>', views.lap_detail, name='detail'),
-   
-]
